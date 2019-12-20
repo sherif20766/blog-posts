814afe63a7a10e395be98c45a4e4ee85d7b7d8ab
sherif20766 <sherif20766@hotmail.com>
2019-12-20 12:06:34 +0000

    created the fetchUser action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,3 +8,12 @@ export const fetchPosts = () => async (dispatch, getState) => {
 		payload: response.data
 	});
 };
+
+export const fetchUser = id => async (dispatch, getState) => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({
+		type: "FETCH_USER",
+		payload: response.data
+	});
+};
