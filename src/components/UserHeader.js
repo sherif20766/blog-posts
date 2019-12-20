de800d2fea71acb2f0cf5120bf969306d3e13eb0
sherif20766 <sherif20766@hotmail.com>
2019-12-20 14:55:55 +0000

    UserHeader component configuration continued

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
+	componentDidMount() {
+		this.props.fetchUser(this.props.userId);
+	}
 	render() {
 		return <div>Header</div>;
 	}
 }
 
-export default UserHeader;
+const mapStateToProps = state => {
+	return { user: state.user };
+};
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
