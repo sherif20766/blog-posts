ccb461fa83bb4dd679f975bfe017bfe937a35023
sherif20766 <sherif20766@hotmail.com>
2019-12-20 15:21:02 +0000

    completed the UserHeader component config

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,11 +7,19 @@ class UserHeader extends React.Component {
 		this.props.fetchUser(this.props.userId);
 	}
 	render() {
-		return <div>Header</div>;
+		const user = this.props.users.find(
+			user => user.id === this.props.userId
+		);
+
+		if (!user) {
+			return null;
+		}
+
+		return <div className="header">{user.name}</div>;
 	}
 }
 
 const mapStateToProps = state => {
-	return { user: state.user };
+	return { users: state.users };
 };
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
