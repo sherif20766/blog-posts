2ef754470b57b48ca60d599f2e56a856996ed3b3
sherif20766 <sherif20766@hotmail.com>
2019-12-20 17:58:24 +0000

    used memoization to solive issue of duplicate network requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async (dispatch, getState) => {
@@ -9,11 +10,15 @@ export const fetchPosts = () => async (dispatch, getState) => {
 	});
 };
 
-export const fetchUser = id => async (dispatch, getState) => {
+export const fetchUser = id => (dispatch, getState) => {
+	_fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
 	const response = await jsonPlaceholder.get(`/users/${id}`);
 
 	dispatch({
 		type: "FETCH_USER",
 		payload: response.data
 	});
-};
+});
